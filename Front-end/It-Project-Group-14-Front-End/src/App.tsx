624565d31718b226cf894c45ffd8bad66fe697a4
JiaYiTong23 <jttong2@student.unimelb.edu.au>
2025-09-17 13:08:20 +1000

    Create components "add filters" in search page
    Current version of the add filters
    - when the add filters button is clicked the overlay will pop up
    - the overlay includes three filters: business size, industry, location
    - each filter has its own dropdown bar which shows the options available
    - the dropdown bars are scrollable
    - when the overlay is closed, the filter wont disappear, it is stored, so the user can go back and check what filter has been applied
    - the user can deselect the options by "select {filter.title}" or just refresh the page
    
    things to implement:
    - implement the filter button (send data back to backend and fetch data)

--- Front-end/It-Project-Group-14-Front-End/src/App.tsx
+++ Front-end/It-Project-Group-14-Front-End/src/App.tsx
@@ -1,25 +1,42 @@
 import { useState } from 'react'
 //import './App.css'
 import './index.css';
-import { SearchBar } from './components/SearchBar'
-import { SearchResultList } from './components/SearchResultList';
+import { SearchBar } from './components/searchBar/SearchBar'
+import { SearchResultList } from './components/searchBar/SearchResultList';
+import { FilterMetrics } from './components/filterFinancialMetrics/FilterMetrics';
+import FilterSearchPage from './components/filterSearchPage/FilterSearchPage';
 
   type Company = {
     id : number;
     name : string;
   }
 
+  const  METRIC_DATA = [
+    {id: "1", value :"Metric 1"},
+    {id: "2", value :"Metric 2"},
+    {id: "3", value :"Metric 3"},
+  ]
+
 function App() {
   const [results, setResults] = useState<Company[]>([]);
+
+  const handleSelect = (e:React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value, e.target.checked);
+  }
 
   return (
     <>
       <div className="w-screen h-screen bg-white flex justify-center items-start">
+        {/* Search bar and results */}
         <div className="flex flex-col items-center w-full max-w-[1180px] min-w-[300px] pt-[20vh] px-4">
           <SearchBar setResults = {setResults}></SearchBar>
           <SearchResultList results = {results}></SearchResultList>
+           {/* <FilterMetrics list = {METRIC_DATA} handleSelect={{handleSelect}}></FilterMetrics> */}
+           <FilterMetrics buttonText = "Dropdown button" content={<p>Hello world!!!</p>}></FilterMetrics>
+           <FilterSearchPage></FilterSearchPage>
         </div>
       </div>
+      
     </>
   )
 }
---
+++ Front-end/It-Project-Group-14-Front-End/src/components/filterSearchPage/FilterSearchPage.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import FilterButton from './FilterButton'
+import Overlay from './Overlay'
+
+const FilterSearchPage = () => {
+
+    const [showOverlay, setShowOverlay] = useState(false);
+
+    const [filters, setFilters] = useState({
+        businessSize : "",
+        revenue : "",
+        employees: "",
+    })
+
+    return (
+        <div>
+            <FilterButton onClick={()=> setShowOverlay(true)}></FilterButton>
+            {showOverlay && 
+            <Overlay 
+              onClose={()=>setShowOverlay(false)}
+              filters = {filters}
+              setFilters = {setFilters}>
+            </Overlay>}
+        </div>
+    )
+}
+
+export default FilterSearchPage
