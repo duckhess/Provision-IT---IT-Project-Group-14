11c64bf48260830d83bb831740a5ba2feb3bd066
duckhess <mdjaja@student.unimelb.edu.au>
2025-09-26 14:07:19 +1000

    Revert "merging branch for abs benchmarking"

--- Back-end/routes/abs_benchmarkings.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import abs_controller from '../controllers/abs_controller.js'
-
-const router = express.Router()
-
-router.get('/', abs_controller.fetch_abs)
-
-export default router
