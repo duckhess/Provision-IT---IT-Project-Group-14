c672168ab5f8dfa566da2a5c922070d0dcd7f673
Anderson-Yong <andersonyong@student.unimelb.edu.au>
2025-10-01 10:41:03 +1000

    Added company card to carousel

--- Front-end/It-Project-Group-14-Front-End/src/pages/HomePage.tsx
+++ Front-end/It-Project-Group-14-Front-End/src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import companyData from "../data/CompanyData.json"
 import Summary from "../components/BasicSummary"
 
