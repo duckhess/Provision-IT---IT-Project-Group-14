1916f9af329f37853d5a4116cd8b62462527dcd3
duckhess <mdjaja@student.unimelb.edu.au>
2025-10-19 22:52:39 +1100

    debug wcm field not showing

--- Back-end/services/wcm_service.js
+++ Back-end/services/wcm_service.js
@@ -9,41 +9,54 @@ export async function filter_wcm(filters = {}) {
     if (filters.capitalid) matching_params.CapitalID = Number(filters.capitalid)
     if (filters.applicationid) matching_params.ApplicationID = Number(filters.applicationid)
     if (filters.fileid) matching_params.FileID = Number(filters.fileid)
-    
-    const value = await wcm_values_schema.find(matching_params).lean()
-
-    const forecast_filters = value.map(v => ({
-        CapitalID: v.CapitalID,
-        ApplicationID: v.ApplicationID
-    }))
-
-    const all_forecast = await wcm_forecasts_schema.find({$or: forecast_filters}).lean()
-    const mapped_forecast = new Map()
-    all_forecast.forEach(f => {
-        const key = `${f.CapitalID}_${f.ApplicationID}`
-        mapped_forecast.set(key, f)
-    })
 
     const document = await wcm_schema.find().lean()
     const mapped_document = new Map()
     document.forEach(w => mapped_document.set(w.CapitalID, w))
+    
+    // const value = await wcm_values_schema.find(matching_params).lean()
+
+    // const forecast_filters = value.map(v => ({
+    //     CapitalID: v.CapitalID,
+    //     ApplicationID: v.ApplicationID
+    // }))
+
+    // const all_forecast = await wcm_forecasts_schema.find({$or: forecast_filters}).lean()
+    // const mapped_forecast = new Map()
+    // all_forecast.forEach(f => {
+    //     const key = `${f.CapitalID}_${f.ApplicationID}`
+    //     mapped_forecast.set(key, f)
+    // })
+
+    const forecast = await wcm_forecasts_schema.find(matching_params).lean()
+    const forecast_filters = forecast.map(d => ({
+        CapitalID: d.CapitalID,
+        ApplicationID: d.ApplicationID
+    }))
+
+    const value = await wcm_values_schema.find({$or: forecast_filters}).lean()
+    const mapped_value = new Map()
+    value.forEach(f => {
+        const key = `${f.CapitalID}`
+        mapped_value.set(key, f)
+    })
 
     const fileIDs = [...new Set(value.map(v => v.FileID))]
     const timelineMap = await get_period(fileIDs)
 
-    return value.map(v => {
-        const movements = mapped_document.get(v.CapitalID)
-        const key = `${v.CapitalID}_${v.ApplicationID}`
-        const forecast = mapped_forecast.get(key)
+    return forecast.map(f => {
+        const movements = mapped_document.get(f.CapitalID)
+        const key = `${f.CapitalID}`
+        const v = mapped_value.get(key)
         return {
-            CapitalID: v.CapitalID,
+            CapitalID: f.CapitalID,
             Metric: movements.Metric,
             Unit: movements.Unit,
-            ApplicationID: v.ApplicationID,
-            Timeline: timelineMap.get(v.FileID),
-            Value: parseFloat(v.Value),
-            "Avg Historical Forecast": parseFloat(forecast?.["Avg Historical Forecast"].toString()),
-            "User Forecast": parseFloat(forecast?.["User Forecast"].toString())
+            ApplicationID: f.ApplicationID,
+            Timeline: timelineMap.get(v?.FileID),
+            Value: parseFloat(v?.Value),
+            "Avg Historical Forecast": parseFloat(f?.["Avg Historical Forecast"].toString()),
+            "User Forecast": parseFloat(f?.["User Forecast"].toString())
         }
     })
 
