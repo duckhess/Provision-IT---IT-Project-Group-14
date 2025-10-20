b7f979c7b875d86d9906c40809b37df8d4d8c114
JiaYiTong23 <jttong2@student.unimelb.edu.au>
2025-10-21 10:53:01 +1100

    Set up testing env

--- Front-end/vite.config.ts
+++ Front-end/vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    proxy : {
-      "/api" : {
-        target : 'http://localhost:3000',
-        changeOrigin:true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      }
-    }
-  }
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+
+  test: {
+    environment: 'jsdom', 
+    globals: true,        
+    setupFiles: './src/setupTests.ts', 
+    css: true,            
+  },
 });
